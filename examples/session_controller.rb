5d21aec717594df14f68aebf23be2c9870e761ef
Mikkel Malmberg <mikkelmalmberg@me.com>
2015-02-26 13:36:38 +0100

    add session_controller example

--- examples/session_controller.rb
+++ examples/session_controller.rb
@@ -1,1 +1,16 @@
-# TODO
+class SessionController < ApplicationController
+  skip_user_authentication only: [:new]
+
+  def new
+  end
+
+  def create
+    if user = Autho::Authentication.new(User, params[:email], params[:password]).user
+      Autho::UserSession.new(self, :user_id, User).user = user
+      redirect_to root_path, flash: { notice: t(:"sessions.successfully_signed_in") }
+    else
+      redirect_to :back, flash: { error: t(:"sessions.bad_credentials") }
+    end
+  end
+end
+
